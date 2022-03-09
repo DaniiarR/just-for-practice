f61adff9ffee30555ec38d36ca1144c359225906
daniiarr <danraiymkulov@mail.com>
2022-03-09 22:48:16 +0900

    implemented dateTime chip

--- app/src/main/java/com/example/justforpractice/tasks/add/AddTaskModalBottomSheet.kt
+++ app/src/main/java/com/example/justforpractice/tasks/add/AddTaskModalBottomSheet.kt
@@ -9,6 +9,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.CalendarView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import com.example.justforpractice.R
@@ -16,6 +18,7 @@ import com.example.justforpractice.databinding.ChooseDateModalBinding
 import com.example.justforpractice.databinding.FragmentAddTaskModalBottomSheetBinding
 import com.example.justforpractice.tasks.list.TaskListViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
@@ -48,13 +51,45 @@ class AddTaskModalBottomSheet : BottomSheetDialogFragment() {
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
             showKeyboard(binding.addTaskEt)
         }
+        viewModel.createTask()
+
         binding.chooseDateButton.setOnClickListener {
             showDatePicker()
+            setupDateObserver(binding.dateTimeChip)
+            setupTimeObserver(binding.dateTimeChip)
+            binding.dateTimeChip.setOnCloseIconClickListener { viewModel.deleteDateTime() }
         }
-        viewModel.createTask()
+        binding.addDescriptionButton.setOnClickListener {
+            binding.addDescriptionTiL.visibility = View.VISIBLE
+            binding.addDescriptionEt.requestFocus()
+        }
+        binding.addTaskEt.doOnTextChanged { text, _, _, _ ->
+            viewModel.setTaskName(text.toString())
+        }
+        binding.addDescriptionEt.doOnTextChanged { text, _, _, _ ->
+            viewModel.setTaskDescription(text.toString())
+        }
     }
 
-    private fun setupObserver(addTaskTimeEt: TextInputEditText) {
+    private fun setupDateObserver(chip: Chip) {
+        viewModel.dateToAdd.observe(viewLifecycleOwner) {
+            if (it != null){
+                viewModel.addDateToTask(it)
+                chip.visibility = View.VISIBLE
+                chip.text = viewModel.getFormattedDateTime()
+            } else {
+                chip.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setupTimeObserver(chip: Chip) {
+        viewModel.timeToAdd.observe(viewLifecycleOwner) {
+            it?.let { chip.text = viewModel.getFormattedDateTime() }
+        }
+    }
+
+    private fun setupTimeObserver(addTaskTimeEt: TextInputEditText) {
         viewModel.timeToAdd.observe(viewLifecycleOwner) {
             it?.let {
                 viewModel.addTimeToTask(it)
@@ -74,14 +109,12 @@ class AddTaskModalBottomSheet : BottomSheetDialogFragment() {
         builder.setView(binding.root)
         val dialog = builder.create()
         dialog.show()
-        setupObserver(binding.addTaskTimeEt)
-        viewModel.setDate(LocalDate.now())
+        setDate(binding.addTaskCalendarView)
+        setupTimeObserver(binding.addTaskTimeEt)
         binding.addTaskDateCancelButton.setOnClickListener { dialog.dismiss() }
-        binding.addTaskDateDoneButton.setOnClickListener {
-            //TODO Somehow save the date
-        }
+        binding.addTaskDateDoneButton.setOnClickListener { dialog.dismiss() }
         binding.addTaskCalendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            viewModel.setDate(LocalDate.of(year, month + 1, dayOfMonth))
+            viewModel.setDate(year, month, dayOfMonth)
         }
         binding.addTaskTimeEt.setOnClickListener {
             showTimePicker()
@@ -89,13 +122,21 @@ class AddTaskModalBottomSheet : BottomSheetDialogFragment() {
 
     }
 
+    private fun setDate(calendarView: CalendarView) {
+        viewModel.getDate()?.let {
+            calendarView.date = it
+            return
+        }
+        viewModel.setDate(LocalDate.now())
+    }
+
     private fun showTimePicker() {
         val isSystem24Hour = is24HourFormat(requireContext())
         val clockFormat = if (isSystem24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
         val picker = MaterialTimePicker.Builder()
                 .setTimeFormat(clockFormat)
-                .setHour(OffsetTime.now().hour)
-                .setMinute(OffsetTime.now().minute)
+                .setHour(viewModel.getHours() ?: OffsetTime.now().hour)
+                .setMinute(viewModel.getMinutes() ?: OffsetTime.now().minute)
                 .build()
         picker.show(parentFragmentManager, null)
         picker.addOnPositiveButtonClickListener {
@@ -115,6 +156,7 @@ class AddTaskModalBottomSheet : BottomSheetDialogFragment() {
     override fun onCancel(dialog: DialogInterface) {
         super.onCancel(dialog)
         viewModel.deleteNewTask()
+        viewModel.deleteDateTime()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/justforpractice/tasks/list/TaskListViewModel.kt
+++ app/src/main/java/com/example/justforpractice/tasks/list/TaskListViewModel.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.launch
 import org.threeten.bp.LocalDate
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.OffsetTime
+import org.threeten.bp.format.DateTimeFormatter
 import java.lang.Exception
 
 class TaskListViewModel(private val repository: AppRepository) : ViewModel() {
@@ -19,6 +20,7 @@ class TaskListViewModel(private val repository: AppRepository) : ViewModel() {
     private val tasks = MutableLiveData<Resource<List<Task>>>()
 
     private val taskToAdd = MutableLiveData<Task?>()
+    val dateToAdd = MutableLiveData<LocalDate?>()
     val timeToAdd = MutableLiveData<OffsetTime?>()
 
     init {
@@ -41,15 +43,40 @@ class TaskListViewModel(private val repository: AppRepository) : ViewModel() {
         taskToAdd.value = Task()
     }
 
-    fun setDate(date: LocalDate) {
-        taskToAdd.value?.date = date
+    fun setDate(year: Int, month: Int, dayOfMonth: Int) {
+        dateToAdd.value = LocalDate.of(year, month + 1, dayOfMonth)
     }
+
+    fun setDate(date: LocalDate?) {
+        dateToAdd.value = date
+    }
+
+    fun getDate(): Long? = taskToAdd.value?.date?.atStartOfDay()?.toInstant(OffsetDateTime.now().offset)?.toEpochMilli()
+
+    fun getFormattedDateTime(): String? {
+        val formatter = DateTimeFormatter.ofPattern("EEE, MMM dd")
+        var dateTime = formatter.format(taskToAdd.value?.date)
+        timeToAdd.value?.let {
+            dateTime += ", " + DateTimeFormatter.ofPattern("HH:mm").format(it)
+        }
+        return dateTime
+    }
+
+    fun getHours(): Int? = taskToAdd.value?.time?.hour
+
+    fun getMinutes(): Int? = taskToAdd.value?.time?.minute
 
     fun getTasks(): LiveData<Resource<List<Task>>> = tasks
 
     fun deleteNewTask() {
         taskToAdd.value = null
+    }
+
+    fun deleteDateTime() {
+        taskToAdd.value?.date = null
+        taskToAdd.value?.time = null
         timeToAdd.value = null
+        dateToAdd.value = null
     }
 
     fun setTime(time: OffsetTime) {
@@ -59,4 +86,19 @@ class TaskListViewModel(private val repository: AppRepository) : ViewModel() {
     fun addTimeToTask(time: OffsetTime) {
         taskToAdd.value?.time = time
     }
+
+    fun addDateToTask(date: LocalDate) {
+        taskToAdd.value?.date = date
+    }
+
+    fun setTaskName(name: String) {
+        taskToAdd.value?.name = name
+    }
+
+    fun setTaskDescription(description: String) {
+        taskToAdd.value?.additionalInfo = description
+    }
+
+
+
 }
